cd3b31c53bae34a6a2eeb02845d47d388b3e4615
ARC Labs Studio <arclabs.studio@gmail.com>
2026-01-23 09:41:24 +0100

    Initial commit of ARCAuthentication package

---
+++ Tests/ARCAuthenticationTests/ARCAuthenticationTests.swift
@@ -0,0 +1,9 @@
+import Testing
+@testable import ARCAuthentication
+
+struct ARCAuthenticationTests {
+    @Test
+    func testHelloFunction() {
+        #expect(ARCAuthentication.hello() == "Hello from ARCAuthentication!")
+    }
+}

9453bb319dea5648f3357eefd59b42e339dcddb8
ARC Labs Studio <arclabs.studio@gmail.com>
2026-01-23 12:10:52 +0100

    chore: remove placeholder files
    - Delete initial placeholder ARCAuthentication.swift
    - Delete initial placeholder test file
    - Package now uses proper modular structure

--- Tests/ARCAuthenticationTests/ARCAuthenticationTests.swift
+++
@@ -1,9 +0,0 @@
-import Testing
-@testable import ARCAuthentication
-
-struct ARCAuthenticationTests {
-    @Test
-    func testHelloFunction() {
-        #expect(ARCAuthentication.hello() == "Hello from ARCAuthentication!")
-    }
-}
